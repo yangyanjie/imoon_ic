5db2e6d0fd370d7684f503d7cf590b3a42e5a858
yangyang <yangyanjie@ochain.fun>
2018-12-13 17:12:43 +0800

    add-js

--- js/navigators/AppNavigator.js
+++ js/navigators/AppNavigator.js
@@ -1,30 +1,54 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import WelcomePage from '../page/WelcomePage';
-import HomePage from '../page/HomePage';
-import TrendingPage from '../page/TrendingPage';
-import FavoritePage from '../page/FavoritePage';
-import MyPage from '../page/my/MyPage';
-import RepositoryDetail from '../page/RepositoryDetail';
+import WelcomePage from '../page/WelcomePage'
+import HomePage from '../page/HomePage'
+import RepositoryDetail from '../page/RepositoryDetail'
+import SearchPage from '../page/SearchPage'
+import FavoritePage from '../page/FavoritePage'
+import WebViewPage from '../page/WebViewPage'
+import CustomKeyPage from '../page/my/CustomKeyPage'
+import CustomTheme from '../page/my/CustomTheme'
+import MyPage from '../page/my/MyPage'
+import SortKeyPagePage from '../page/my/SortKeyPagePage'
+import AboutMePage from '../page/about/AboutMePage'
+import AboutPage from '../page/about/AboutPage'
 
 const StackNavigator = createStackNavigator({
     WelcomePage: {
         screen: WelcomePage
     },
     HomePage: {
-        screen: HomePage,
-    },
-    TrendingPage: {
-        screen: TrendingPage,
-    },
-    FavoritePage: {
-        screen: FavoritePage,
-    },
-    MyPage: {
-        screen: MyPage,
+        screen: HomePage
     },
     RepositoryDetail: {
         screen: RepositoryDetail
     },
+    SearchPage: {
+        screen: SearchPage
+    },
+    CustomKeyPage: {
+        screen: CustomKeyPage
+    },
+    FavoritePage: {
+        screen: FavoritePage
+    },
+    WebViewPage: {
+        screen: WebViewPage
+    },
+    CustomTheme: {
+        screen: CustomTheme
+    },
+    MyPage: {
+        screen: MyPage
+    },
+    SortKeyPagePage: {
+        screen: SortKeyPagePage
+    },
+    AboutMePage: {
+        screen: AboutMePage
+    },
+    AboutPage: {
+        screen: AboutPage
+    }
 },{
     defaultNavigationOptions: {
         header:null
